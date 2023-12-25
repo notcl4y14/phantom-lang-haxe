218976d0d966989e427d72c60a58b06397d4ab6e
notcl4y14 <notcl4y@gmail.com>
2023-12-25 17:32:02 +0300

    Added file reader and tests folder

--- Phantom.hx
+++ Phantom.hx
@@ -6,24 +6,56 @@ class Phantom {
 
 		Sys.println(lines);
 	}
-	
-	public function new() {
-		var args = Sys.args();
 
-		if (
-			args.length == 0 ||
-			args.contains("--help")
-		) {
-			Phantom.usage();
-			return;
-		}
-
-		var lexer = new Lexer("+-*/%^= += -= *= /= %= ^= .,:;&|! ()[]{} 0.5+2 `\"lol 'a'\"` x1 lol_foobar true false let var");
+	public function run(code: String) {
+		var lexer = new Lexer(code);
 		var tokens = lexer.lexerize();
 
 		for (token in tokens) {
 			Sys.println(token.string());
 		}
+	}
+	
+	public function new() {
+		var args = Sys.args();
+
+		// Checking for some arguments
+		if (args.contains("--help")) {
+			Phantom.usage();
+			return;
+		}
+
+		// Setting variables
+		var filename: String = null;
+
+		// Iterating arguments
+		for (arg in args) {
+			if (arg.substring(0, 1) == "-") {
+				continue;
+			}
+
+			if (filename != null) {
+				Sys.println("Filename already specified!");
+				return;
+			}
+
+			filename = arg;
+		}
+
+		if (filename == null) {
+			Sys.println("Please specify a filename!");
+			return;
+		}
+
+		if (!sys.FileSystem.exists(filename)) {
+			Sys.println('${filename} does not exist!');
+			return;
+		}
+
+		var code = sys.io.File.read(filename)
+			.readAll().toString();
+
+		this.run(code);
 	}
 
 	static public function main() {
